179ace6d46295fb6a6c1006b7b8e83c0200611cb
Reggie <reggiest09@gmail.com>
2016-06-17 23:10:15 -0400

    comment raven out

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,7 +1,7 @@
-require 'raven'
+# require 'raven'
 
-if Rails.env.production?
-  Raven.configure do |config|
-    config.dsn = ENV['SENTRY_DNS']
-  end
-end
+# if Rails.env.production?
+#   Raven.configure do |config|
+#     config.dsn = ENV['SENTRY_DNS']
+#   end
+# end
